2a5789ab473ca8830e0f0e4bacc53371b50f8354
Vicky Garcia Hernandez <gar_vicky93@hotmail.com>
2018-09-22 19:04:16 -0500

    Cambios

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -1,14 +1,14 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Inicio extends CI_Controller {
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Inicio extends CI_Controller {
 	function __construct(){
 		parent::__construct();
     $this->load->helper(array('url'));
 	}
-
-	function index(){
-    $data['title'] = ucfirst('Inicio');
+	public function index()
+	{
+		$data['title'] = ucfirst('Inicio');
     $this->load->view('templates/header',$data);
     $this->load->view('pages/inicio');
     $this->load->view('templates/footer');
