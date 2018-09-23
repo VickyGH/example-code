5382bfcd47a4726c14529c6c89922e83ec1d493c
Vicky Garcia Hernandez <gar_vicky93@hotmail.com>
2018-09-22 23:37:40 -0500

    a

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,18 +3,55 @@ class Pages extends CI_Controller {
   function __construct(){
 		parent::__construct();
     $this->load->helper(array('url'));
+
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('nombre', 'Nombre', 'required|min_length[10]');
+		$this->form_validation->set_rules('correo', 'Correo', 'required|min_length[10]');
+    $this->form_validation->set_rules('comentario', 'Comentario', 'required|min_length[10]');
+
+    if($this->form_validation->run() === true){
+        //Si la validación es correcta, cogemos los datos de la variable POST
+        //y los enviamos al modelo
+        $nombre = $this->input->post('nombre');
+				$correo = $this->input->post('correo');
+        $comentario = $this->input->post('comentario');
+
+        $this->load->model('Directorio_model');
+        $this->Directorio_model->insertar_comentario($nombre, $correo, $comentario);
+    }
+
 	}
 
   public function view($page = 'home'){
-  if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php')){
-          // Whoops, we don't have a page for that!
-          show_404();
+    if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php')){
+            // Whoops, we don't have a page for that!
+            show_404();
+    }
+
+    $data['title'] = ucfirst($page); // Capitalize the first letter
+
+    $this->load->view('templates/header', $data);
+    $this->load->view('pages/'.$page, $data);
+    $this->load->view('templates/footer', $data);
   }
 
-  $data['title'] = ucfirst($page); // Capitalize the first letter
+  public function cargar(){
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('nombre', 'Nombre', 'required|min_length[10]');
+		$this->form_validation->set_rules('correo', 'Correo', 'required|min_length[10]');
+    $this->form_validation->set_rules('comentario', 'Comentario', 'required|min_length[10]');
 
-  $this->load->view('templates/header', $data);
-  $this->load->view('pages/'.$page, $data);
-  $this->load->view('templates/footer', $data);
-}
+    if($this->form_validation->run() === true){
+        //Si la validación es correcta, cogemos los datos de la variable POST
+        //y los enviamos al modelo
+        $nombre = $this->input->post('nombre');
+				$correo = $this->input->post('correo');
+        $comentario = $this->input->post('comentario');
+
+        $this->load->model('Directorio_model');
+        $this->Directorio_model->insertar_comentario($nombre, $correo, $comentario);
+    }
+	}
 }
