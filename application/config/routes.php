91bdf89394c1b7ed2ac36193d22093f4042d903e
Vicky Garcia Hernandez <gar_vicky93@hotmail.com>
2018-09-22 20:29:36 -0500

    a

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,8 +50,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 //$route['default_controller'] = 'pages/inicio';
-$route['default_controller'] = 'inicio';
+//$route['default_controller'] = 'inicio';
 $route['beneficiarios'] = 'beneficiarios/index';
+
+$route['default_controller'] = 'pages/view';
+$route['(:any)'] = 'pages/view/$1';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
